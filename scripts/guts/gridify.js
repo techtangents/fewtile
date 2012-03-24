cb77e6c2834ad920dfc9f9782455022a4a8dd8c5
dylan <dylan@godric>
2012-03-24 20:53:27 +1000

    grid stuff

--- scripts/guts/gridify.js
+++ scripts/guts/gridify.js
@@ -1,46 +1,12 @@
-define(['guts/comparison'], function(comparison) {
+define(['guts/comparison', 'guts/griddle'], function(comparison, griddle) {
   var arrayMin = comparison.arrayMin;
   var by = comparison.by;
 
   return function(aspectRatio) {
     return function(totalWidth, totalHeight, numCells) {
-
-      var calc = function(rows) {
-        var normalCols = Math.floor(rows / numCells);
-        var skinnyCols = normalCols + 1;
-
-        var skinnyRows = rows % numCells;
-        var normalRows = rows - skinnyRows;
-        
-        var height = totalHeight / rows;
-
-        var f = function(r, c) {
-          var cells = r * c;
-          var width = totalWidth / c;
-          var ar = width / height;
-
-          return {
-            cells: cells,
-            width: width,
-            height: height,
-            ar: ar
-          };
-        };
-
-        var normal = f(normalRows, normalCols);
-        var skinny = f(skinnyRows, skinnyCols);
-
-        var meanAr = (normal.ar * normal.cells + skinny.ar * skinny.cells) / numCells;
-
-        return {
-          rows: rows,
-          normal: normal,
-          skinny: skinny,
-          meanAr: meanAr
-        }
-      };
-
-      var layouts = _(numCells).map(calc);
+      var r = _.range(1, numCells + 1);
+      var g = griddle(totalWidth, totalHeight, numCells);
+      var layouts = _.map(r, g);
 
       var deltaAr = function(x) {
         return Math.abs(x.meanAr - aspectRatio);
--- test/t/test_gridify.js
+++ test/t/test_gridify.js
@@ -2,8 +2,11 @@ define(['guts/gridify'], function(gridify) {
   return function(assert) {
     var ar = 7/2;
 
-    assert.deepEqual(gridify(ar)(100, 200, 1), {
-      
+    assert.deepEqual(gridify(ar)(100, 200, 1), { 
+      rows: 1,
+      normal: { cells: 1, width: 100, height: 200, ar: 0.5 },
+      skinny: { cells: 0, width: 50, height: 200, ar: 0.25 },
+      meanAr: 0.5
     });
   };
 });
