a73fa2eee182b8f39ed1b692ef5063c52e3010b8
dylan <dylan@godric>
2012-05-06 23:57:45 +1000

    added jquery color plugin for color animations

--- scripts/fewtile.js
+++ scripts/fewtile.js
@@ -1,8 +1,9 @@
 require.config({
   paths: {
-    "jquery"     : "lib/jquery-1.7.1.min",
-    "underscore" : "lib/underscore",
-    "ba-resize"  : "lib/jquery.ba-resize.min"
+    "jquery"      : "lib/jquery-1.7.1.min",
+    "underscore"  : "lib/underscore",
+    "ba-resize"   : "lib/jquery.ba-resize.min",
+    'jquery-color': 'lib/jquery.color'
   }
 });
 
