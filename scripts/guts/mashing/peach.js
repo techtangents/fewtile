ff40762b0d63d2ee7d43f08d42ee99c483585da9
Jacob Stanley <jacob@stanley.io>
2015-12-08 15:00:40 +1100

    ftfy

--- scripts/guts/mashing/peach.js
+++
@@ -1,9 +0,0 @@
-define(['guts/mashing/parmap'], function(parmap) {
-  return function(fs, callback) {
-    parmap(fs, function(x, cb) {
-      x(cb);
-    }, function(_) {
-      callback();
-    });
-  };
-});
--- test/t/test_outerJoin.js
+++
@@ -1,8 +0,0 @@
-define(['guts/mashing/outerJoin'], function(outerJoin) { 
-  return function(assert) {
-    assert.deepEqual({}, outerJoin({}, {}));
-    assert.deepEqual({a: {left:1, right:2}}, outerJoin({a: 1}, {a: 2}));
-    assert.deepEqual({a: {left:1}}, outerJoin({a: 1}, {}));
-    assert.deepEqual({a: {right:2}}, outerJoin({}, {a: 2}));
-  }; 
-});
